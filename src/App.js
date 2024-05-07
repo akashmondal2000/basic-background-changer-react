de2095ad685d73077341be60c3d54057cce98c8b
akashmondal2000 <mondalakash494@gmail.com>
2024-05-07 18:44:33 +0530

    remove condition and set color on button callback function

--- src/App.js
+++ src/App.js
@@ -4,41 +4,56 @@ import "./App.css";
 const App = () => {
   const [color, setColor] = useState("white");
 
-  const chooseColor = (color) =>{
-    if(color === "red" ){
-      setColor("red")
-    }else if(color === "blue"){
-      setColor("blue");
-    }else if (color === "green"){
-      setColor("green")
-    }else if( color === "black"){
-      setColor("black")
-    }else{
-      setColor("white")
-    }
-  }
-
-
   return (
     <div className="container" style={{ backgroundColor: color }}>
       <div className="colorBar">
         <div className="barWrapper">
-          <button onClick={()=>{chooseColor("red")}} className="colorBtn" style={{ backgroundColor: "red" }}>
+          <button
+            onClick={() => {
+              setColor("red");
+            }}
+            className="colorBtn"
+            style={{ backgroundColor: "red" }}
+          >
             red
           </button>
 
-          <button onClick={()=>{chooseColor("blue")}} className="colorBtn" style={{ backgroundColor: "blue" }}>
+          <button
+            onClick={() => {
+              setColor("blue");
+            }}
+            className="colorBtn"
+            style={{ backgroundColor: "blue" }}
+          >
             blue
           </button>
 
-          <button onClick={()=>{chooseColor("green")}} className="colorBtn" style={{ backgroundColor: "green" }}>
+          <button
+            onClick={() => {
+              setColor("green");
+            }}
+            className="colorBtn"
+            style={{ backgroundColor: "green" }}
+          >
             green
           </button>
 
-          <button onClick={()=>{chooseColor("black")}} className="colorBtn" style={{ backgroundColor: "black" }}>
+          <button
+            onClick={() => {
+              setColor("black");
+            }}
+            className="colorBtn"
+            style={{ backgroundColor: "black" }}
+          >
             black
           </button>
-          <button onClick={()=>{chooseColor("white")}} className="colorBtn" style={{ backgroundColor: "white", color: "black" }}>
+          <button
+            onClick={() => {
+              setColor("white");
+            }}
+            className="colorBtn"
+            style={{ backgroundColor: "white", color: "black" }}
+          >
             white
           </button>
         </div>
